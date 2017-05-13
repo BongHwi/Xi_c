bb45f83daef372c4ca38be723825d9aab972fde2
BongHwi <bonghwilim@gmail.com>
2017-05-13 12:03:05 +0900

    change file name

---
+++ AliAnalysisTaskXic.h
@@ -0,0 +1,38 @@
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. */
+/* See cxx source for full Copyright notice */
+/* $Id$ */
+
+#ifndef AliAnalysisTaskXic_H
+#define AliAnalysisTaskXic_H
+
+#include "AliAnalysisTaskSE.h"
+#include "AliESDpid.h"
+
+class AliAnalysisTaskXic : public AliAnalysisTaskSE  
+{
+    public:
+                                AliAnalysisTaskXic();
+                                AliAnalysisTaskXic(const char *name);
+        virtual                 ~AliAnalysisTaskXic();
+
+        virtual void            UserCreateOutputObjects();
+        virtual void            UserExec(Option_t* option);
+        virtual void            Terminate(Option_t* option);
+
+    private:
+        AliESDEvent*            fESD;         //! input event
+        TList*                  fOutputList;  //! output list
+        AliPIDResponse*         fPIDResponse; //! PID object
+        AliCentrality*          fCentrality;  //! Centrality object
+        AliESDtrackCuts        *fTrackCut;   //! ESD track cuts
+
+
+        TH1F*                   fHistPt;        //! dummy histogram
+
+        AliAnalysisTaskXic(const AliAnalysisTaskXic&); // not implemented
+        AliAnalysisTaskXic& operator=(const AliAnalysisTaskXic&); // not implemented
+
+        ClassDef(AliAnalysisTaskXic, 1);
+};
+
+#endif
