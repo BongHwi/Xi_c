c8501bd00583ad60bed8c89d0c13f5abe246cb97
Bong-Hwi Lim <bonghwilim@gmail.com>
2017-06-10 00:37:55 +0900

    debuging

--- AliAnalysisTaskXic.cxx
+++ AliAnalysisTaskXic.cxx
@@ -736,8 +736,8 @@ void AliAnalysisTaskXic::UserExec(Option_t *)
     for (Int_t iV0 = 0; iV0 < nv0s; iV0++){
       for (Int_t jV0 = iV0; jV0 < nv0s; jV0++){
         if(v0checklam[iV0]) cout << iV0 << " Lambda: "<< v0checklam[iV0] << endl;
-        if(v0checkk0s[iV0]) cout << jV0 << " K0s: " << v0checkk0s[iV0] << endl;
-        //cout << "Lambda: "<< v0checklam[iV0] << " K0s: "<< v0checkk0s[iV0] << endl;
+        //if(v0checkk0s[iV0]) cout << jV0 << " K0s: " << v0checkk0s[iV0] << endl;
+        cout << "Lambda: "<< v0checklam[iV0] << " K0s: "<< v0checkk0s[iV0] << endl;
         if(!(v0checklam[iV0] && v0checkk0s[iV0])) continue;
         AliESDv0 *v0i = ((AliESDEvent*)fESD)->GetV0(iV0);
         AliESDv0 *v0j = ((AliESDEvent*)fESD)->GetV0(jV0);
