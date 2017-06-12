6c967f3ccf41f61bb9ed37368fc1046cad0e314f
Bong-Hwi Lim <bonghwilim@gmail.com>
2017-06-12 21:15:59 +0900

    add get result macro

---
+++ macros/getResult.C
@@ -0,0 +1,10 @@
+void getResult(){
+    TFile *filein = TFile::Open(ss+"/AnalysisResults.root","READ");
+    //TFile *filein = TFile::Open("001/AnalysisResults.root","READ");
+    if(filein==NULL) continue;
+    TDirectoryFile *dir = (TDirectoryFile*)filein->Get("Xic");
+    TList *list_qa = (TList*)dir->Get("QA");
+    TList *list_hist = (TList*)dir->Get("Histograms");
+    (TH1F*)list_qa->FindObject("fInvLambda")->Draw();
+    c1->SaveAs("./Figs/Lambda_Invmass.png");
+}
